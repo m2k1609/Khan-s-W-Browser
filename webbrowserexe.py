ee9f7579442a6b8c49be4369fa7405f774547eb3
m2k1609 <m2k1609@gmail.com>
2025-07-19 12:03:27 -0400

    vtrl based structure testing page

---
+++ webbrowserexe.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = 'ilikecats'  # Change this to a random secret key
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
